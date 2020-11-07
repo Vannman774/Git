adacb62a80821ac919b7bd3f659668eeded23b2d
Vannman774 <vann.opperman@gmail.com>
2020-11-07 12:57:43 -0500

    Edited

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ function setup(){
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
 
-    box3 = new Box(700,240,70,70);
+    box3 = new Box(701,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
 
